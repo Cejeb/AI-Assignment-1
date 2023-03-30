35a3876b6bf7f50df68dce6839485c277836b00c
Cejeb <cameronjbone1@gmail.com>
2023-03-30 15:42:45 +0100

    Object name fixes
    Fixed some object names so I remember this code, going to swap to a collision and button press to buy items within the world, but leaving this commented out as backup

--- player-shop.cpp
+++ player-shop.cpp
@@ -2,34 +2,40 @@
 
 int currency = 0;
 
-int shop(void)
+//This version uses a window pop up, but we think an ingame system similar to Spelunky would suit better, leaving this commented out for now
+/*int shop(void)
 {
     const int screenWidth = 640;
     const int screenHeight = 480;
 
     InitWindow(screenWidth, screenHeight, "Menu Window");
 
-    Rectangle button1Rec = { screenWidth / 2 - 50, 200, 100, 40 };
-    Rectangle button2Rec = { screenWidth / 2 - 50, 260, 100, 40 };
+    Rectangle BuyButton = { screenWidth / 2 - 50, 200, 100, 40 };
+    Rectangle ExitButton = { screenWidth / 2 - 50, 260, 100, 40 };
 
     SetTargetFPS(60);
 
     while (!WindowShouldClose())
     {
-        if (CheckCollisionPointRec(GetMousePosition(), button1Rec) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
-            break;
+        if (CheckCollisionPointRec(GetMousePosition(), BuyButton) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+            if (currency >= 20)
+            {
+                currency = currency - 20;
+            }
+        }
 
-        if (CheckCollisionPointRec(GetMousePosition(), button2Rec) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+        if (CheckCollisionPointRec(GetMousePosition(), ExitButton) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
             break;
+        }
 
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
-        DrawRectangleRec(button1Rec, GRAY);
-        DrawText("Button 1", button1Rec.x + 10, button1Rec.y + 10, 20, WHITE);
+        DrawRectangleRec(BuyButton, GRAY);
+        DrawText("Buy Health Potion", BuyButton.x + 10, BuyButton.y + 10, 20, WHITE);
 
-        DrawRectangleRec(button2Rec, GRAY);
-        DrawText("Button 2", button2Rec.x + 10, button2Rec.y + 10, 20, WHITE);
+        DrawRectangleRec(ExitButton, GRAY);
+        DrawText("Leave Shop", ExitButton.x + 10, ExitButton.y + 10, 20, WHITE);
 
         EndDrawing();
     }
@@ -37,4 +43,4 @@ int shop(void)
     CloseWindow();
 
     return 0;
-}
+}*/
